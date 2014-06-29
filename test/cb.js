2886b2b6620d28c6bd654f5d7ae1e8199736b003
James Halliday <mail@substack.net>
2014-06-30 01:20:19 +0800

    more cb test fixes, more cases

--- test/cb.js
+++ test/cb.js
@@ -1,9 +1,10 @@
 var dataplex = require('../');
+var Readable = require('readable-stream').Readable;
 var test = require('tape');
 var concat = require('concat-stream');
 
 test('cb', function (t) {
-    t.plan(4);
+    t.plan(6);
     
     var plex1 = dataplex();
     var plex2 = dataplex();
@@ -12,18 +13,34 @@ test('cb', function (t) {
         cb(null, Array(Number(opts.n)+1).join('A'));
     });
     
+    plex1.add('/bbb/:n?', function (opts) {
+        var r = new Readable;
+        r._read = function () {};
+        r.push(Array(Number(opts.n)+1).join('B'));
+        r.push(null);
+        return r;
+    });
+    
     plex2.open('/aaa/5').pipe(concat(function (body) {
         t.equal(body.toString('utf8'), 'AAAAA');
     }));
-    plex1.get('/aaa/5').pipe(concat(function (body) {
-        t.equal(body.toString('utf8'), 'XYZ');
+    plex1.get('/aaa/3').pipe(concat(function (body) {
+        t.equal(body.toString('utf8'), 'AAA');
     }));
     
     plex2.open('/aaa/5', function (err, body) {
         t.equal(body.toString('utf8'), 'AAAAA');
     });
-    plex1.get('/aaa/5', function (err, body) {
-        t.equal(body.toString('utf8'), 'XYZ');
+    plex1.get('/aaa/3', function (err, body) {
+        t.equal(body.toString('utf8'), 'AAA');
+    });
+    
+    plex1.get('/bbb/5', function (err, body) {
+        t.equal(body.toString('utf8'), 'BBBBB');
+    });
+    
+    plex2.open('/bbb/5', function (err, body) {
+        t.equal(body.toString('utf8'), 'BBBBB');
     });
     
     plex1.pipe(plex2).pipe(plex1);
