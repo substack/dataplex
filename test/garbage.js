2ccade2658fe723cf0922df79956b73a43b2bced
James Halliday <mail@substack.net>
2014-08-10 22:39:30 -0700

    garbage coverage of the case where the connection is already closed

--- test/garbage.js
+++ test/garbage.js
@@ -14,3 +14,17 @@ test('garbage in', function (t) {
     plex.write('yoyoyo\n');
     plex.resume();
 });
+
+test('garbage already ended', function (t) {
+    t.plan(1);
+    
+    var plex = dataplex();
+    plex.add('/xyz', function (opts) {});
+    
+    plex.on('end', function () {
+        t.ok(true, 'hung up');
+    });
+    plex.write('yoyoyo\n');
+    plex._mdm.end();
+    plex.resume();
+});
