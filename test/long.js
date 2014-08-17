70c7a16fd3375c5f5f7e68554e7436678396b502
James Halliday <mail@substack.net>
2014-08-16 18:41:20 -0700

    chunking the long test, still works

--- test/long.js
+++ test/long.js
@@ -3,16 +3,22 @@ var Readable = require('readable-stream').Readable;
 var test = require('tape');
 var concat = require('concat-stream');
 
+var chunky = require('chunky');
+
 test('long string', function (t) {
     t.plan(1);
     var payload = Buffer(Array(50000+1).join('A'));
+    var chunks = chunky(payload);
     
     var plex1 = dataplex();
     plex1.add('/long', function (opts) {
         var s = new Readable;
         s._read = function () {};
-        s.push(payload);
-        s.push(null);
+        (function next () {
+            if (chunks.length === 0) return s.push(null);
+            s.push(chunks.shift());
+            setTimeout(next, 10);
+        })();
         return s;
     });
     
