d3a9a96ce8d54ebe4aea856f4c3b430a7e8cad8e
James Halliday <mail@substack.net>
2014-10-01 17:54:26 -0700

    error without close

---
+++ test/close_error.js
@@ -0,0 +1,41 @@
+var dataplex = require('../');
+var Writable = require('readable-stream').Writable;
+var test = require('tape');
+var concat = require('concat-stream');
+
+test('close event on destroy', function (t) {
+    t.plan(2);
+    
+    var events = [];
+    var buffers = [];
+    var plex1 = dataplex();
+    var plex2 = dataplex();
+    
+    plex1.add('/xyz', function (opts) {
+        var s = new Writable;
+        s._write = function (buf, enc, next) {
+            buffers.push(buf);
+            next();
+        };
+        process.nextTick(function () {
+            s.emit('error', new Error('yo'));
+        });
+        s.on('error', function (err) {
+            events.push('error:' + err.message);
+        });
+        s.on('_close', function () {
+            events.push('_close');
+        });
+        return s;
+    });
+    
+    var stream = plex2.open('/xyz');
+    stream.end('yo');
+    
+    plex1.pipe(plex2).pipe(plex1);
+    
+    setTimeout(function () {
+        t.deepEqual(buffers, []);
+        t.deepEqual(events, [ 'err:yo', '_close' ]);
+    }, 20);
+});
