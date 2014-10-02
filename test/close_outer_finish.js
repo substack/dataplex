2e8ede1307dcdf3b3f3ef0177e532647fa7728c6
James Halliday <mail@substack.net>
2014-10-02 07:45:22 -0700

    testing the close on outer finish case

---
+++ test/close_outer_finish.js
@@ -0,0 +1,41 @@
+var dataplex = require('../');
+var Writable = require('readable-stream').Writable;
+var test = require('tape');
+var concat = require('concat-stream');
+
+test('close event on outer finish', function (t) {
+    t.plan(2);
+    
+    var events = [];
+    var buffers = [];
+    var plex1 = dataplex();
+    var plex2 = dataplex();
+    
+    plex1.add('/xyz', function (opts) {
+        var s = new Writable;
+        s._write = function (buf, enc, next) {
+            buffers.push(buf);
+            next();
+        };
+        s.on('_close', function () {
+            events.push('_close');
+        });
+        s.on('finish', function () {
+            events.push('finish');
+        });
+        return s;
+    });
+    
+    var stream = plex2.open('/xyz');
+    stream.write('yo');
+    
+    plex1.pipe(plex2).pipe(plex1);
+    setTimeout(function () {
+        plex1.end();
+    }, 5);
+    
+    setTimeout(function () {
+        t.equal(Buffer.concat(buffers).toString('utf8'), 'yo');
+        t.deepEqual(events, [ '_close' ]);
+    }, 20);
+});
